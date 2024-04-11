6e86d8f855294ed4c3a086f7a15b299d7de13c42
valery <tikavalery@gmail.com>
2024-04-10 20:24:14 -0700

    fixed bugs in stripe functionality

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -2,33 +2,73 @@ const express = require("express");
 const router = express.Router();
 const Booking = require("../models/bookingModel");
 const Car = require("../models/carModel")
-
+const stripe = require("stripe")("sk_test_51NovQALXB4fdf1TyG6VvOSCzPBNhv9MR0KmSRgk31AAPullpsqd5u2A5Rz8fUCxf2SmkIcYoWAEFbfWZA4L9ehOv00IoXlM4PG")
+const { v4: uuidv4 } = require('uuid');
 
 router.post("/bookcar",async(req,res) =>{
+console.log("i am in book car route")
 
-    req.body.transactionId = "1234"
+    const {token} = req.body
+    
     try {
-        console.log(req.body)
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            customer_email:token.email,
+            line_items: [{
+              price_data:{
+                currency:"usd",
+                product_data:{
+                  name:token.email,
+
+                },
+                unit_amount:req.body.totalAmount,
+              },
+              quantity:100
+                                                                            // price:token.id,
+                                                                            // quantity: req.body.totalAmount * 100
+                                                                            
+                                                                          //     email:token.email,
+                                                                          // source:token.id,
+                                                                          //  amount:req.body.totalAmount * 100,
+                                                                          // customer:customer.id,
+                                                                          // receipt_email: token.email
+            }],
+            mode: 'payment',
+            success_url: 'https://appwexdigital.com/',
+            cancel_url: 'https://appwexdigital.com/',
+          });
+          // console.log("before session")
+          // console.log(session)
+          // console.log("after session")
+        // const customer = await stripe.customers.create({
+        //     email:token.email,
+        //     source:token.id
+        // })
+
+        // const payment = await stripe.charges.create({
+        //     amount:req.body.totalAmount * 100,
+        //     customer:customer.id,
+        //     receipt_email: token.email
+        // },{
+        //     idempotencyKey:uuidv4()
+        // })
+      if(session){
+        // console.log(session)
+        console.log(" I am in payment")
+        req.body.transactionId = session.id
         const newbooking = new Booking(req.body)
         await newbooking.save()
-
-        //  Car.update(
-        //     { _id: req.body.car}, 
-        //     { $push: { bookedTimeSlots: req.body.bookedTimeSlots} }
-        // );
-
-        // const res = await Person.updateOne({ name: 'Jean-Luc Picard' }, { ship: 'USS Enterprise' });
        const res2 = await Car.updateOne({ _id: req.body.car},{ $push: { bookedTimeSlots: req.body.bookedTimeSlots} }
- );
-        // const car = await Car.findOne({_id : req.body.car})
-        // console.log(car)
-        // car.bookedTimeSlots.push(req.body.bookedTimeSlots)
-        // console.log(car)
-    //     await car.save()
-    //    console.log(car)
+               );
+
         res.send("Your booking is successfull")
+      }else{
+        // return res.status(400).json(error);
+      }
+     
     } catch(error){
-        return res.status(400).json(error);
+        // return res.status(400).json(error);
+        console.log(error)
     }
 })
 
