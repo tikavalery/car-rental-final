772ae4f20874366bd6d7d80b6171fff271386fb4
valery <tikavalery@gmail.com>
2024-05-16 12:26:57 -0700

    added changes to server and package.json

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use("/api/cars/", require("./routes/carsRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
 app.use("/api/users/", require("./routes/usersRoute"));
 
+const path = require("path")
 
+if(process.env.NODE_ENV === "production")
+    {
+        app.use("/", express.static("client/build"))
+        app.get("*", (req,res) => {
+            res.sendFile(path.resolve(__dirname,"client/build/index.html"))
+        })
+    }
 
 // mongoose.connect(MONGOURI);
 // mongoose.connection.on("connected",()=>{
